90819df830974fe131c02f37e7e4a2b4351796ed
Steve Bertrand <steveb@cpan.org>
2020-03-29 14:00:52 -0700

    Updated example

--- examples/RelayData/RelayData.ino
+++ examples/RelayData/RelayData.ino
@@ -1,57 +1,24 @@
-/*********
-    Rui Santos
-    Complete project details at https://RandomNerdTutorials.com
-*********/
-
-#include "FS.h"
-#include <ArduinoJson.h>
+#include <RelayData.h>
 
 void setup() {
     Serial.begin(9600);
 
-    DynamicJsonDocument json = fetchData();
-    writeData(json);
-}
+    // Retrieve the stored data file
 
-void writeData (DynamicJsonDocument doc) {
-    SPIFFS.remove("/config.json");
-    
+    DynamicJsonDocument doc = fetchData();
+
+    /* Make some changes */
+    // doc["relays"][0]["temp"] = 90;
+
+    // Write the file back to flash
+
+    writeData(doc);
+
+    // Print the data to the serial monitor
+
     serializeJsonPretty(doc, Serial);
-
-    doc["relays"][0]["temp"] = 78;
-    
-    File configFile = SPIFFS.open("/config.json", "w");
-
-    if (configFile) {
-        serializeJson(doc, configFile);
-        configFile.close();
-    }
-    serializeJsonPretty(doc, Serial);
-
 }
 
-DynamicJsonDocument fetchData () {
-    if (SPIFFS.begin()) {
-        if (SPIFFS.exists("/config.json")) {
-            File configFile = SPIFFS.open("/config.json", "r");
-
-            DynamicJsonDocument doc(1024);
-
-            if (configFile) {
-
-                auto error = deserializeJson(doc, configFile);
-
-                if (error) {
-                    Serial.println(error.c_str());
-                    configFile.close();
-                }
-                else {    
-                    return doc;
-                }
-            }
-        }
-    }
-}
 void loop() {
 
 }
